7e2bcee2cc1c601f9c30d311b123fb8261ff6faa
James Collier <jrc.collier@gmail.com>
2014-03-30 17:40:03 +0100

    Clean up grid functionality

--- src/main/java/com/gubbns/gollns/Grid.java
+++ src/main/java/com/gubbns/gollns/Grid.java
@@ -1,43 +1,41 @@
 package com.gubbns.gollns;
 
-
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Grid {
     private final Set<XY> aliveCells;
 
-    public Grid() {
-        this(Collections.<XY>emptySet());
-    }
-
     public Grid(Set<XY> aliveCells) {
         this.aliveCells = Collections.unmodifiableSet(aliveCells);
     }
 
-    public int countAliveNeighbours(XY cell) {
-        int numNeighbours = 0;
-        for (NeighbourCell neighbour : NeighbourCell.values()) {
-            if (isCellAlive(neighbour.getNeighbourPos(cell))) {
-                numNeighbours++;
-            }
-        }
-        return numNeighbours;
+
+    public Set<XY> findAliveCellsWithNumNeighbours(Integer... numNeighbours) {
+        return findCellsWithNumNeighbours(CellState.ALIVE, numNeighbours);
     }
 
+    public Set<XY> findDeadCellsWithNumNeighbours(Integer numNeighbours) {
+        return findCellsWithNumNeighbours(CellState.DEAD, numNeighbours);
+    }
+
+    private Set<XY> findCellsWithNumNeighbours(CellState cellState, Integer... numNeighbours) {
+        final Set<Integer> numNeighboursToMatch = new HashSet<>(Arrays.asList(numNeighbours));
+        return getNeighbourCountMap().entrySet().stream()
+                .filter(entry -> getCellState(entry.getKey()).equals(cellState))
+                .filter(entry -> numNeighboursToMatch.contains(entry.getValue()))
+                .map(Map.Entry<XY, Integer>::getKey)
+                .collect(Collectors.toSet());
+    }
+
+    private CellState getCellState(XY cell) {
+        return isCellAlive(cell) ? CellState.ALIVE : CellState.DEAD;
+    }
 
     private boolean isCellAlive(XY cell) {
         return aliveCells.contains(cell);
     }
 
-    public Set<XY> findAliveCellsWithNeighbours(Integer... numNeighbours) {
-        final Set<Integer> numNeighboursToMatch = new HashSet<>(Arrays.asList(numNeighbours));
-
-        return getNeighbourCountMap().entrySet().stream()
-                .filter(entry -> numNeighboursToMatch.contains(entry.getValue()))
-                .map(Map.Entry<XY, Integer>::getKey)
-                .collect(Collectors.toSet());
-    }
 
     private Map<XY, Integer> getNeighbourCountMap() {
         Map<XY, Integer> neighbourCountMap = new HashMap<>();
@@ -55,12 +53,8 @@ public class Grid {
     }
 
     private Map<XY, Integer> addNeighbourCount(Map<XY, Integer> neighbourCountMap, XY neighbourCell) {
-        if (isCellAlive(neighbourCell)) {
-            Integer neighbourCount = neighbourCountMap.getOrDefault(neighbourCell, 0) + 1;
-            neighbourCountMap.put(neighbourCell, neighbourCount);
-        }
+        Integer neighbourCount = neighbourCountMap.getOrDefault(neighbourCell, 0) + 1;
+        neighbourCountMap.put(neighbourCell, neighbourCount);
         return neighbourCountMap;
     }
-
-
 }
